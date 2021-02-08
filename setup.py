c6b7518076cbc6c886b4aa22311abe9bd3b8a39f
Daniil <danedbalabanov@gmail.com>
2021-02-08 22:04:20 +0000

    Fix setup.py

--- setup.py
+++ setup.py
@@ -1,15 +1,15 @@
-from distutils.core import setup
+from setuptools import setup, find_packages
 
 setup(name='insider_trader_bot',
       version='1.0',
       description='Telegram bot for insider trading',
       author='Daniil Balabanov',
       author_email='danedbalabanov@gmail.com',
-      packages=['distutils',
-                'distutils.command',
+      packages=find_packages(exclude=['env', 'tests']),
+      install_requirements=[
                 "user_agent",
                 "requests",
                 "beautifulsoup4",
                 "python-telegram-bot==13.2"
-                ],
+                ]
      )
